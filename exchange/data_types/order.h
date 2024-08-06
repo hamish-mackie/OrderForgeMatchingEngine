ae5bfd346dcf479d14a8bc7638033bd7068835b1
hamish <hamish.g.mackie@gmail.com>
2024-08-06 21:32:29 +1000

    add fmt, magic enum

--- exchange/data_types/order.h
+++ exchange/data_types/order.h
@@ -2,6 +2,7 @@
 
 #include "time_utils.h"
 #include "enums.h"
+#include "logger/logger.h"
 
 class Order {
 public:
@@ -15,14 +16,20 @@ public:
           timestamp_(get_nano_ts()),
           acc_id_(acc_id) { }
 
-    [[nodiscard]] const Price & price() const { return price_; }
-    [[nodiscard]] const Quantity & qty() const { return qty_; }
+    [[nodiscard]] const Price price() const { return price_; }
+    [[nodiscard]] const Quantity qty() const { return qty_; }
     [[nodiscard]] Side side() const { return side_; }
     [[nodiscard]] OrderStatus status() const { return status_; }
     [[nodiscard]] OrderType type() const { return type_; }
     [[nodiscard]] OrderId order_id() const { return order_id_; }
     [[nodiscard]] clock_t timestamp() const { return timestamp_; }
     [[nodiscard]] uint64_t acc_id() const { return acc_id_; }
+
+    std::string log_order() const {
+        return fmt::format("Price: {}, Quantity: {}, Side: {}, Status: {}, Type: {}, AccountId: {}, OrderId: {}, Timestamp: {}",
+                  price_.descaled_value(), qty_.descaled_value(), magic_enum::enum_name(side_),
+                  magic_enum::enum_name(status_), magic_enum::enum_name(type_), acc_id_, order_id_, timestamp_);
+    }
 
 private:
     Price price_;
