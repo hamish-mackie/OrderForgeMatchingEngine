b5c108426642f8fe06fd3d09acfea334b3e67293
hamish <hamish.g.mackie@gmail.com>
2024-08-06 17:46:18 +1000

    fix build, add test

--- exchange/tests/test_book_side.cpp
+++ exchange/tests/test_book_side.cpp
@@ -3,11 +3,11 @@
 #include "book_side.h"
 
 TEST(BookSide, Construct) {
-    BookSide bs = BookSide(Side::BUY, Price(100), Price(0.01));
+    auto bs = BookSide(Side::BUY, Price(100), Price(0.01));
     ASSERT_EQ(bs.start_price().descaled_value(), 95);
     bs.end_price();
 
-    BookSide bs2 = BookSide(Side::SELL, Price(100), Price(0.01));
+    auto bs2 = BookSide(Side::SELL, Price(100), Price(0.01));
     ASSERT_EQ(bs2.start_price().descaled_value(), 105);
     bs.end_price();
 
