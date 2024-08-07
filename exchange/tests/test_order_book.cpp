032e2ab0e633170d8b1e79a23fcfc29ed5863f5e
hamish <hamish.g.mackie@gmail.com>
2024-08-08 08:40:13 +1000

    tidy tests for orderbook

--- exchange/tests/test_order_book.cpp
+++ exchange/tests/test_order_book.cpp
@@ -7,10 +7,7 @@ public:
     OrderBookTest()
         : start_price_(100)
         , tick_size_(0.01)
-        , ob(start_price_, tick_size_) {
-
-
-    }
+        , ob(start_price_, tick_size_) {}
 protected:
     void SetUp() override {
         ob.private_order_update_handler = [&](const Order& order) { order_updates.push_back(order); };
@@ -20,9 +17,6 @@ protected:
         ob.public_last_trade_update_handler = [&](const LastTradeUpdate& update) { last_trade_updates_.push_back(update); };
     }
 
-    void TearDown() override {
-
-    }
     Price start_price_;
     Price tick_size_;
     OrderBook ob;
@@ -32,169 +26,115 @@ protected:
     std::vector<LastTradeUpdate> last_trade_updates_;
 };
 
-void verify_order(Order& sent, Order& received) {
-    ASSERT_EQ(sent.price().descaled_value(), received.price().descaled_value());
-    ASSERT_EQ(sent.qty().descaled_value(), received.qty().descaled_value());
-    ASSERT_EQ(sent.status(), received.status());
-    ASSERT_EQ(sent.type(), received.type());
-    ASSERT_EQ(sent.side(), received.side());
-    ASSERT_EQ(sent.order_id(), received.order_id());
+void verify_order(Order& result, Order& received) {
+    ASSERT_EQ(result.price().descaled_value(), received.price().descaled_value());
+    // check sent qty vs remaining qty so we can make a result order to test against.
+    ASSERT_EQ(result.qty().descaled_value(), received.remaining_qty().descaled_value());
+    ASSERT_EQ(result.status(), received.status());
+    ASSERT_EQ(result.type(), received.type());
+    ASSERT_EQ(result.side(), received.side());
+    ASSERT_EQ(result.order_id(), received.order_id());
 }
 
-void verify_level_update(LevelUpdate& update, Price price, Quantity total_qty, Side side) {
-    ASSERT_EQ(update.price.descaled_value(), price.descaled_value());
-    ASSERT_EQ(update.total_quantity.descaled_value(), total_qty.descaled_value());
-    ASSERT_EQ(update.side, side);
+void verify_level_update(LevelUpdate& result, LevelUpdate& received) {
+    ASSERT_EQ(result.price.descaled_value(), received.price.descaled_value());
+    ASSERT_EQ(result.total_quantity.descaled_value(), received.total_quantity.descaled_value());
+    ASSERT_EQ(result.side, received.side);
 }
 
-TEST_F(OrderBookTest, test_send_limit_orders) {
-    auto order1 = Order(Price(99.3), Quantity(1), BUY, OPEN, LIMIT, 555, 1);
-    ob.add_order(order1);
-    verify_order(order1, order_updates[0]);
-    verify_level_update(level_updates_[0], order1.price(), order1.qty(), order1.side());
+void verify_level_update(LevelUpdate& result, Price price, Quantity total_qty, Side side) {
+    auto received = LevelUpdate(price, total_qty, side);
+    verify_level_update(result, received);
 }
 
-TEST(OrderBookTest, test_send_market_orders) {
-
-
-}
-
-
-
-TEST(OrderBook, test_simple_trade) {
-    auto starting_price = Price(100);
-    auto tick_size = Price(0.1);
-    OrderBook ob(starting_price, tick_size);
-    ob.public_order_book_update_handler = [](const LevelUpdate& update) {};
-
-
-    ob.private_trades_update_handler = [&](const Trade& trade) {
-        ASSERT_EQ(trade.crossing_side(), SELL);
-        ASSERT_EQ(trade.price(), Price(99.7));
+TEST_F(OrderBookTest, test_send_and_remove_limit_orders) {
+    std::vector<Order> orders = {
+        Order(Price(99.3),  Quantity(0.1),  BUY,  OPEN, LIMIT, 555, 1),
+        Order(Price(99.5),  Quantity(0.2),  BUY,  OPEN, LIMIT, 555, 2),
+        Order(Price(99.1),  Quantity(1),    BUY,  OPEN, LIMIT, 555, 3),
+        Order(Price(100),   Quantity(1),    SELL, OPEN, LIMIT, 555, 4),
+        Order(Price(100.05),Quantity(5),    SELL, OPEN, LIMIT, 555, 5),
+        Order(Price(100.6), Quantity(8),    SELL, OPEN, LIMIT, 555, 6)
     };
 
-    std::vector<Order> limit_orders = {
-        Order(Price(99.3), Quantity(1), BUY, OPEN, LIMIT, 555, 1),
-        Order(Price(99.5), Quantity(1), BUY, OPEN, LIMIT, 555, 2),
-        Order(Price(99.7), Quantity(1), BUY, OPEN, LIMIT, 555, 3),
+    std::vector<LevelUpdate> expected_level_updates = {
+        LevelUpdate(Price(99.3),    Quantity(0.1),  BUY),
+        LevelUpdate(Price(99.5),    Quantity(0.2),  BUY),
+        LevelUpdate(Price(99.1),    Quantity(1),    BUY),
+        LevelUpdate(Price(100),     Quantity(1),    SELL),
+        LevelUpdate(Price(100.05),  Quantity(5),    SELL),
+        LevelUpdate(Price(100.6),   Quantity(8),    SELL),
+        LevelUpdate(Price(99.3),    Quantity(0),    BUY),
+        LevelUpdate(Price(99.5),    Quantity(0),    BUY),
+        LevelUpdate(Price(99.1),    Quantity(0),    BUY),
+        LevelUpdate(Price(100),     Quantity(0),    SELL),
+        LevelUpdate(Price(100.05),  Quantity(0),    SELL),
+        LevelUpdate(Price(100.6),   Quantity(0),    SELL)
     };
-    for (auto& order : limit_orders) {
+
+    for(auto i = 0; i < orders.size(); ++i) {
+        auto order = orders[i];
         ob.add_order(order);
-    }
-
-    std::vector<Order> market_orders = {
-        Order(Price(99.5), Quantity(1), SELL, OPEN, MARKET, 9999, 100),
-    };
-
-    for (auto& order : market_orders) {
-        ob.add_order(order);
+        verify_order(order, order_updates[i]);
+        verify_level_update(level_updates_[i], order.price(), order.qty(), order.side());
     }
 }
 
-TEST(OrderBook, test_simple_trade_other_side) {
-    auto starting_price = Price(100);
-    auto tick_size = Price(0.1);
-    OrderBook ob(starting_price, tick_size);
-    ob.private_account_update_handler = []() {};
-    ob.public_order_book_update_handler = [](const LevelUpdate& update) {};
-
-    ob.private_trades_update_handler = [&](const Trade& trade) {
-        ASSERT_EQ(trade.crossing_side(), BUY);
-        ASSERT_EQ(trade.price(), Price(25.2));
+TEST_F(OrderBookTest, test_send_market_orders) {
+    std::vector<Order> orders = {
+        Order(Price(98),    Quantity(5), BUY,  OPEN, LIMIT, 555, 1),
+        Order(Price(99),    Quantity(5), BUY,  OPEN, LIMIT, 555, 2),
+        Order(Price(97),    Quantity(5), BUY,  OPEN, LIMIT, 555, 3),
+        Order(Price(100),   Quantity(5), SELL, OPEN, LIMIT, 555, 4),
+        Order(Price(101),   Quantity(5), SELL, OPEN, LIMIT, 555, 5),
+        Order(Price(105),   Quantity(5), SELL, OPEN, LIMIT, 555, 6)
     };
 
-    std::vector<Order> limit_orders = {
-        Order(Price(25.5), Quantity(1), SELL, OPEN, LIMIT, 555, 1),
-        Order(Price(25.5), Quantity(1), SELL, OPEN, LIMIT, 555, 2),
-        Order(Price(25.2), Quantity(1), SELL, OPEN, LIMIT, 555, 3),
-        Order(Price(25.6), Quantity(1), SELL, OPEN, LIMIT, 555, 2),
+    std::vector<LevelUpdate> expected_level_updates {
+        LevelUpdate(Price(98),  Quantity(5), BUY),
+        LevelUpdate(Price(99),  Quantity(5), BUY),
+        LevelUpdate(Price(97),  Quantity(5), BUY),
+        LevelUpdate(Price(100), Quantity(5), SELL),
+        LevelUpdate(Price(101), Quantity(5), SELL),
+        LevelUpdate(Price(105), Quantity(5), SELL),
+
+        // Sell Order
+        LevelUpdate(Price(100), Quantity(0), SELL),
+        LevelUpdate(Price(101), Quantity(0), SELL),
+        LevelUpdate(Price(105), Quantity(0), SELL),
+        LevelUpdate(Price(106), Quantity(5), BUY),
+        LevelUpdate(Price(106), Quantity(0), BUY),
+
+        // Buy O
+        LevelUpdate(Price(99),  Quantity(0), BUY),
+        LevelUpdate(Price(98),  Quantity(0), BUY),
+        LevelUpdate(Price(97),  Quantity(0), BUY),
+        LevelUpdate(Price(90),  Quantity(85),SELL),
+        LevelUpdate(Price(90),  Quantity(0), SELL)
     };
-    for (auto& order : limit_orders) {
+
+    for(auto i = 0; i < orders.size(); ++i) {
+        auto order = orders[i];
         ob.add_order(order);
+        verify_order(order, order_updates[i]);
+        verify_level_update(level_updates_[i], order.price(), order.qty(), order.side());
     }
 
-    std::vector<Order> market_orders = {
-        Order(Price(99.5), Quantity(1), BUY, OPEN, MARKET, 9999, 100),
-    };
+    auto market_buy = Order(Price(106), Quantity(20), BUY, OPEN, LIMIT, 555, 8);
+    auto market_buy_result = Order(Price(106), Quantity(5), BUY, PARTIAL, LIMIT, 555, 8);
+    ob.add_order(market_buy);
+    verify_order(market_buy_result, order_updates.back());
+    ob.remove_order(8);
 
-    for (auto& order : market_orders) {
-        ob.add_order(order);
+    auto market_sell = Order(Price(90), Quantity(100), SELL, OPEN, LIMIT, 555, 9);
+    auto market_sell_result = Order(Price(90), Quantity(85), SELL, PARTIAL, LIMIT, 555, 9);
+    ob.add_order(market_sell);
+    verify_order(market_sell_result, order_updates.back());
+    ob.remove_order(9);
+
+    ASSERT_EQ(expected_level_updates.size(), level_updates_.size());
+
+    for(int i = 0; i < expected_level_updates.size(); ++i) {
+        verify_level_update(expected_level_updates[i], level_updates_[i]);
     }
-}
-
-TEST(OrderBook, test_simple_remove_order) {
-    auto starting_price = Price(100);
-    auto tick_size = Price(0.1);
-    OrderBook ob(starting_price, tick_size);
-    ob.private_account_update_handler = []() {};
-    ob.public_order_book_update_handler = [](const LevelUpdate& update) {};
-
-    ob.private_trades_update_handler = [&](const Trade& trade) {
-        ASSERT_EQ(trade.crossing_side(), BUY);
-        ASSERT_EQ(trade.price(), Price(25.2));
-    };
-
-    std::vector<Order> limit_orders = {
-        Order(Price(25.5), Quantity(1), SELL, OPEN, LIMIT, 555, 1),
-        Order(Price(25.5), Quantity(1), SELL, OPEN, LIMIT, 555, 2),
-    };
-    for (auto& order : limit_orders) {
-        ob.add_order(order);
-    }
-
-    ob.remove_order(1);
-}
-
-TEST(OrderBook, test_updates_large_order_placement) {
-    auto starting_price = Price(100);
-    auto tick_size = Price(0.1);
-    OrderBook ob(starting_price, tick_size);
-    ob.private_order_update_handler = [](const Order& order) {};
-    ob.private_account_update_handler = []() {};
-    ob.private_trades_update_handler = [&](const Trade& trade) { };
-    ob.public_order_book_update_handler = [](const LevelUpdate& update) {};
-    ob.public_last_trade_update_handler = [](const LastTradeUpdate& update) {};
-
-    std::vector<Order> limit_orders = {
-        Order(Price(25.5), Quantity(1), SELL, OPEN, LIMIT, 555, 1),
-        Order(Price(26.5), Quantity(1), SELL, OPEN, LIMIT, 555, 2),
-        Order(Price(27.5), Quantity(1), SELL, OPEN, LIMIT, 555, 3),
-        Order(Price(28.5), Quantity(2), SELL, OPEN, LIMIT, 555, 4),
-        Order(Price(29.5), Quantity(1), SELL, OPEN, LIMIT, 555, 5),
-        Order(Price(30.1), Quantity(1), SELL, OPEN, LIMIT, 555, 6)
-    };
-
-    for (auto& order : limit_orders) {
-        ob.add_order(order);
-    }
-
-    auto market_order = Order(Price(99.5), Quantity(10), BUY, OPEN, LIMIT, 9999, 100);
-    auto market_order2 = Order(Price(98.5), Quantity(10), BUY, OPEN, LIMIT, 9999, 101);
-    ob.add_order(market_order);
-    ob.add_order(market_order2);
-}
-
-TEST(OrderBook, test_send_limits) {
-    auto starting_price = Price(100);
-    auto tick_size = Price(0.1);
-    OrderBook ob(starting_price, tick_size);
-    std::vector<Order> order_vec;
-    std::vector<LevelUpdate> book_update_vec;
-    ob.private_order_update_handler = [&](const Order& order) {order_vec.push_back(order); };
-    ob.public_order_book_update_handler = [&](const LevelUpdate& update) {book_update_vec.push_back(update); };
-
-    auto order1 = Order(Price(25.5), Quantity(1), SELL, OPEN, LIMIT, 555, 1);
-    ob.add_order(order1);
-    auto order2 = Order(Price(25.5), Quantity(3), SELL, OPEN, LIMIT, 555, 1);
-    ob.add_order(order2);
-
-    ASSERT_EQ(order_vec[0].status(), OPEN);
-    ASSERT_EQ(order_vec[0].price().descaled_value(), 25.5);
-    ASSERT_EQ(book_update_vec[0].price.descaled_value(), 25.5);
-    ASSERT_EQ(book_update_vec[0].total_quantity.descaled_value(), 1);
-
-    ASSERT_EQ(order_vec[1].status(), OPEN);
-    ASSERT_EQ(order_vec[1].price().descaled_value(), 25.5);
-    ASSERT_EQ(book_update_vec[1].price.descaled_value(), 25.5);
-    ASSERT_EQ(book_update_vec[1].total_quantity.descaled_value(), 4);
 }
