45f8e5ca078e1bc00aba8da03bf9f6144ab57bb7
hamish mackie <hamish.g.mackie@gmail.com>
2024-08-13 08:07:40 +1000

    book level tests use fixture

--- OrderForge/tests/test_book_level.cpp
+++ OrderForge/tests/test_book_level.cpp
@@ -3,31 +3,23 @@
 #include "book_level.h"
 
 class TestBookLevel : public ::testing::Test {
-public:
+protected:
     void SetUp() override {
         Logger::get_instance(true, MB * 1, 5);
         REGISTER_TYPE(ORDER, Order);
         REGISTER_TYPE(TRADE, Trade);
+        REGISTER_TYPE(DEBUG, Debug);
     }
 
-private:
     Price bid_price_{50};
     Price ask_price_{100};
     BookLevel book_level_bid_{bid_price_, BUY};
     BookLevel book_level_ask_{ask_price_, SELL};
     std::vector<LevelUpdate> level_updates_;
+    std::pmr::unsynchronized_pool_resource pool{};
 };
 
-TEST_F(TestBookLevel, test_trades_match_in_correct_order) {
-
-}
-
-TEST(TestBookLevel, match_order) {
-    auto pool = std::pmr::unsynchronized_pool_resource();
-    Logger::get_instance(true);
-    REGISTER_TYPE(ORDER, Order);
-
-    auto bl = BookLevel(Price(100), BUY);
+TEST_F(TestBookLevel, match_order) {
 
     std::vector<Order> limit_orders = {
         Order(Price(100), Quantity(1), BUY, OPEN, LIMIT, 9999, 1),
@@ -35,52 +27,49 @@ TEST(TestBookLevel, match_order) {
         Order(Price(100), Quantity(1), BUY, OPEN, LIMIT, 9999, 3),
     };
     for (auto& order : limit_orders) {
-        auto update = bl.add_order(order);
+        auto update = book_level_bid_.add_order(order);
     }
 
     auto market_order = Order(Price(99), Quantity(2), SELL, OPEN, MARKET, 9999, gen_random_order_id());
     auto trade_producer = TradeProducer(market_order, pool);
-    bl.match_order(trade_producer);
+    book_level_bid_.match_order(trade_producer);
 
     ASSERT_EQ(trade_producer.get_modified_orders_().size(), 2);
 }
 
-TEST(TestBookLevel, add_order) {
-    const Price price(45);
+TEST_F(TestBookLevel, add_order) {
     Quantity qty = Quantity(5);
     const OrderId id = gen_random_order_id();
-    auto bl = BookLevel(price, BUY);
-    auto order = Order(price, qty, BUY, OPEN, LIMIT, 12345, id);
+    auto order = Order(bid_price_, qty, BUY, OPEN, LIMIT, 12345, id);
 
-    ASSERT_EQ(bl.total_quantity(), Quantity(0));
-    ASSERT_EQ(bl.size(), 0);
-    auto update = bl.add_order(order);
+    ASSERT_EQ(book_level_bid_.total_quantity(), Quantity(0));
+    ASSERT_EQ(book_level_bid_.size(), 0);
+    auto update = book_level_bid_.add_order(order);
     LOG_INFO("{}", update.log_level_update());
 
-    ASSERT_EQ(update.price, price);
+    ASSERT_EQ(update.price, bid_price_);
     ASSERT_EQ(update.total_quantity, Quantity(5));
-    ASSERT_EQ(bl.size(), 1);
-    ASSERT_EQ(bl.total_quantity(), qty);
+    ASSERT_EQ(book_level_bid_.size(), 1);
+    ASSERT_EQ(book_level_bid_.total_quantity(), qty);
 }
 
-TEST(TestBookLevel, remove_order) {
-    const Price price(100);
+TEST_F(TestBookLevel, remove_order) {
     Quantity qty = Quantity(5);
     const OrderId id = gen_random_order_id();
-    auto bl = BookLevel(price, BUY);
-    auto order = Order(price, qty, BUY, OPEN, LIMIT, 12345, id);
 
-    auto update = bl.add_order(order);
-    ASSERT_EQ(update.price, price);
+    auto order = Order(bid_price_, qty, BUY, OPEN, LIMIT, 12345, id);
+
+    auto update = book_level_bid_.add_order(order);
+    ASSERT_EQ(update.price, bid_price_);
     ASSERT_EQ(update.total_quantity, Quantity(5));
 
-    ASSERT_EQ(bl.size(), 1);
-    ASSERT_EQ(bl.total_quantity(), qty);
+    ASSERT_EQ(book_level_bid_.size(), 1);
+    ASSERT_EQ(book_level_bid_.total_quantity(), qty);
 
-    auto update2 = bl.remove_order(id);
-    ASSERT_EQ(update2.price, price);
+    auto update2 = book_level_bid_.remove_order(id);
+    ASSERT_EQ(update2.price, bid_price_);
     ASSERT_EQ(update2.total_quantity, Quantity(0));
 
-    ASSERT_EQ(bl.size(), 0);
-    ASSERT_EQ(bl.total_quantity(), Quantity(0));
+    ASSERT_EQ(book_level_bid_.size(), 0);
+    ASSERT_EQ(book_level_bid_.total_quantity(), Quantity(0));
 }
