f9da839d3460374d17dfa1f00087489a2a86aa3b
hamish mackie <hamish.g.mackie@gmail.com>
2024-08-13 12:19:04 +1000

    implement symbol in orderbook and order

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 
 int main() {
     Logger::get_instance(true);
-    auto ob = OrderBook(Price(100), TickSize(0.01));
+    std::string_view symbol = "TESTUSD";
+    auto ob = OrderBook(symbol.data(), Price(100), TickSize(0.01));
     ob.private_order_update_handler = [](const Order &order) { };
     ob.private_trades_update_handler = [](const Trade &trade) { };
     ob.public_order_book_update_handler = [](const LevelUpdate &update) { };
     ob.public_last_trade_update_handler = [](const LastTradeUpdate &update) { };
 
     std::vector<Order> demo_orders = {
-        Order(Price(100),  Quantity(10), BUY, OPEN, LIMIT, 000, 1),
-        Order(Price(200),  Quantity(10), SELL, OPEN, LIMIT, 000, 2),
-        Order(Price(100),  Quantity(10), SELL, OPEN, MARKET, 000, 3),
-        Order(Price(200),  Quantity(10), BUY, OPEN, MARKET, 000, 3),
+        Order(symbol, Price(100),  Quantity(10), BUY, OPEN, LIMIT, 000, 1),
+        Order(symbol, Price(200),  Quantity(10), SELL, OPEN, LIMIT, 000, 2),
+        Order(symbol, Price(100),  Quantity(10), SELL, OPEN, MARKET, 000, 3),
+        Order(symbol, Price(200),  Quantity(10), BUY, OPEN, MARKET, 000, 3),
     };
 
     for(auto& order: demo_orders) {
